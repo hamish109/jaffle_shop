5b561b3fceb664b193f50eb15e5f2abbee256ed9
HamishJ <hamish.johns@gmail.com>
2024-02-10 21:56:06 +1300

    add stg_paymentmethod model

---
+++ models/staging/stg_paymentmethod.sql
@@ -0,0 +1,14 @@
+with source as (
+
+    select * from {{ ref('raw_payments') }}
+
+)
+
+select distinct 
+    case when payment_method = 'credit_card' then 1
+    when payment_method = 'coupon' then 2
+    when payment_method = 'bank_transfer' then 3
+    when payment_method = 'gift_card' then 4
+    else '99' end as payment_method_id,
+    payment_method
+from source
